23d8d555c731b46db6a1ddc3c81c2b69be688a82
samcoeur <samcoeur2013@gmail.com>
2024-09-15 22:35:16 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [data , setData] = useState([])
+  useEffect(()=>{
+    
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      d => {
+        setData(d)
+        console.log(data.users)
+        console.log('hellooo')
+      },[]
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {data.map((user , i) =>( <p key={i}>{user}</p>))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
